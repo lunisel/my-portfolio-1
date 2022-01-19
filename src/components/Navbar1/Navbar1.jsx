0816b79c49ef9f9f5d914f3e2d04644a1db77d3f
Luna Sarmiento <selene942008@hotmail.com>
2022-01-19 15:52:37 +0100

    navbar scroll adjusted

--- src/components/Navbar1/Navbar1.jsx
+++ src/components/Navbar1/Navbar1.jsx
@@ -1,76 +1,103 @@
 import { Link } from "react-router-dom";
 import { GoThreeBars, GoX } from "react-icons/go";
-import { useEffect, useState } from "react";
+import React from "react";
 import logo from "../../assets/LogoWOName.png";
 import "./navbar1.css";
 
-const Navbar1 = () => {
-  const [navToggle, setNavToggle] = useState(false);
-  const [visible, setVisible] = useState(true);
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
+export default class Navbar1 extends React.Component {
+  constructor() {
+    super();
+    this.ref = React.createRef();
+    this.state = {
+      show: true,
+      scrollPos: 0,
+      navToggle: false,
+    };
+  }
 
-  const handleScroll = () => {
-    const currentScrollPos = window.pageYOffset;
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("mousedown", this.clickOut);
+  }
 
-    setVisible(
-      (prevScrollPos > currentScrollPos &&
-        prevScrollPos - currentScrollPos > 70) ||
-        currentScrollPos < 10
-    );
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener("mousedown", this.clickOut);
+  }
 
-    setPrevScrollPos(currentScrollPos);
+  handleScroll = () => {
+    this.setState({
+      ...this.state,
+      scrollPos: document.body.getBoundingClientRect().top,
+      show: document.body.getBoundingClientRect().top > this.state.scrollPos,
+    });
   };
 
-  // new useEffect:
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+  clickOut = (e) => {
+    if (!this.ref.current.contains(e.target)) {
+      this.setState({
+        ...this.state,
+        navToggle: false,
+      });
+    }
+  };
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos, visible, handleScroll]);
-
-  return (
-    <nav id="main-nav" style={{ top: visible ? "0" : "-8rem" }}>
-      <div className="container">
-        <div className="logo-container">
-          <img src={logo} alt="logo" />
+  render() {
+    return (
+      <nav id="main-nav" style={{ top: this.state.show ? "0" : "-8rem" }}>
+        <div className="container">
+          <div className="logo-container">
+            <img src={logo} alt="logo" />
+          </div>
+          <GoThreeBars
+            className={this.state.navToggle ? "d-none" : "burger-icon"}
+            onClick={() =>
+              this.setState({
+                ...this.state,
+                navToggle: true,
+              })
+            }
+          />
+          <GoX
+            className={this.state.navToggle ? "close-icon" : "d-none"}
+            onClick={() =>
+              this.setState({
+                ...this.state,
+                navToggle: false,
+              })
+            }
+          />
+          <ul
+            className={
+              this.state.navToggle
+                ? "nav-links-container active"
+                : "nav-links-container"
+            }
+            ref={this.ref}
+          >
+            <li>
+              <a href="#jumbotron" className="nav-link">
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="/" className="nav-link">
+                ABOUT ME
+              </a>
+            </li>
+            <li>
+              <Link to="/" className="nav-link">
+                PORTFOLIO
+              </Link>
+            </li>
+            <li>
+              <a href="#footer" className="nav-link">
+                CONTACT ME
+              </a>
+            </li>
+          </ul>
         </div>
-        <GoThreeBars
-          className={navToggle ? "d-none" : "burger-icon"}
-          onClick={() => setNavToggle(true)}
-        />
-        <GoX
-          className={navToggle ? "close-icon" : "d-none"}
-          onClick={() => setNavToggle(false)}
-        />
-        <ul
-          className={
-            navToggle ? "nav-links-container active" : "nav-links-container"
-          }
-        >
-          <li>
-            <Link to="/" className="nav-link">
-              HOME
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="nav-link">
-              ABOUT ME
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="nav-link">
-              PORTFOLIO
-            </Link>
-          </li>
-          <li>
-            <Link to="/" className="nav-link">
-              CONTACT ME
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar1;
+      </nav>
+    );
+  }
+}
