8a13bbca4deb4aace4796e55ac6b053392ec3b88
Luna Sarmiento <84574252+lunisel@users.noreply.github.com>
2025-01-21 14:29:44 +0100

    Update AboutMe.jsx

--- src/components/About me/AboutMe.jsx
+++ src/components/About me/AboutMe.jsx
@@ -13,24 +13,28 @@ const AboutMe = () => {
           <div className="text-cont">
             <p>
               Hi! I'm a <span>software developer</span> with a passion for
-              crafting engaging, functional, and visually appealing
+              crafting engaging, functional, and visually appealing{" "}
               <span>web</span> and <span>mobile applications</span>. What
-              excites me most about this field is the perfect blend of
+              excites me most about this field is the perfect blend of{" "}
               <span>creativity</span> and <span>logical problem-solving</span>
               —every project is a new challenge to bring ideas to life.
+            </p>
+            <p>
               Currently, I work at <span>Enhancers</span>, where I develop
               applications using <span>React Native</span>, continuously
-              refining my skills in both <span>front-end</span> and
+              refining my skills in both <span>front-end</span> and{" "}
               <span>mobile development</span>. Through my past experiences, I've
-              honed my ability to <span>collaborate effectively in teams</span>
-              and maintain a <span>patient, solution-oriented mindset</span>
-              even under pressure. In May 2021, I joined
-              <span>Strive School</span>, a transformative experience that
-              allowed me to develop numerous <span>team-based</span> and
-              <span>individual projects</span> in a fully
-              <span>remote, international environment</span>. This journey not
-              only strengthened my technical skills but also refined my ability
-              to adapt and excel in dynamic, fast-paced settings.
+              honed my ability to <span>collaborate effectively in teams</span>{" "}
+              and maintain a <span>patient, solution-oriented mindset</span>{" "}
+              even under pressure.
+            </p>
+            <p>
+              In May 2021, I joined <span>Strive School</span>, a transformative
+              experience that allowed me to develop numerous{" "}
+              <span>team-based</span> and <span>individual projects</span> in a
+              fully <span>remote, international environment</span>. This journey
+              not only strengthened my technical skills but also refined my
+              ability to adapt and excel in dynamic, fast-paced settings.
             </p>
             <p>
               Here are a few of the technologies I've been working with
